1982eed13d1e67497bbde1228970580d012270a7
Vlad <vlad14196@gmail.com>
2023-04-10 17:00:52 +0300

    Initial Commit

---
+++ AVPlayer/AVPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVPlayerApp.swift
+//  AVPlayer
+//
+//  Created by user on 10.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct AVPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
